f62b25f3d0194134af5ac2b7f5fdea75a3e762bc
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-24 09:29:11 +0700

    Game Station
    Co-authored-by: putukenzhie@gmail.com <putukenzhie@gmail.com>
    Co-authored-by: HOSANNIA MICHAELA MARSINTA HARDIPUTRI <hosannia.hardiputri@binus.ac.id>

---
+++ Customer.java
@@ -0,0 +1,50 @@
+public class Customer 
+{
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+    private String money;
+
+    public String getId() 
+    {
+        return id;
+    }
+    public void setId(String id) 
+    {
+        this.id = id;
+    }
+    public String getName() 
+    {
+        return name;
+    }
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+    public String getAddress() 
+    {
+        return address;
+    }
+    public void setAddress(String address) 
+    {
+        this.address = address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) 
+    {
+        this.phone = phone;
+    }
+    public String getMoney() 
+    {
+        return money;
+    }
+    public void setMoney(String money) 
+    {
+        this.money = money;
+    }
+
+     
+}
---
+++ Game.java
@@ -0,0 +1,36 @@
+public class Game {
+    private int gameId;
+    private String gameName;
+    private double gamePrice;
+    private String gameGenre;
+
+
+    public int getGameId() {
+        return this.gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameName() {
+        return this.gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public double getGamePrice() {
+        return this.gamePrice;
+    }
+
+    public void setGamePrice(double gamePrice) {
+        this.gamePrice = gamePrice;
+    }
+
+
+    
+
+
+}
